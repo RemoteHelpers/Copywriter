0759ef97d920b5c3dbf291d5aa619911c922f92f
Natalia <melezh.nataliya@gmail.com>
2021-04-02 15:17:09 +0300

    валидация формы

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@ document.querySelector('.menu-icon-wrapper').onclick = function(){
   document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
   document.querySelector('.nav__list-holder').classList.toggle('nav__list-holder--visible');
 };
+
+document.querySelector('.nav__list').onclick = function(){
+  document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+  document.querySelector('.nav__list-holder').classList.toggle('nav__list-holder--visible');
+};
+
+let selector = document.querySelectorAll('input[type="tel"]');
+let im = new Inputmask('(999) 999-99-99');
+im.mask(selector);
